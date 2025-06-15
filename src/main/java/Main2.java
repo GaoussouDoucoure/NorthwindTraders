85c583631d94f3c679751c026dffd71fe2f81d2e
Gaoussou Doucoure <gaousd4@gmail.com>
2025-06-15 12:31:37 -0700

    Used PreparedStatement to fetch data

---
+++ src/main/java/Main2.java
@@ -0,0 +1,31 @@
+import java.sql.*;
+import java.util.Scanner;
+
+public class Main2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String url = "jdbc:mysql://localhost:3306/northwind";
+        System.out.print("Enter Database username: ");
+        String username = sc.nextLine();
+        System.out.print("Enter Database password: ");
+        String password = sc.nextLine();
+
+        String query = "SELECT * FROM northwind.products WHERE ProductID = ?";
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection connection = DriverManager.getConnection(url, username, password);
+            PreparedStatement preparedStatement = connection.prepareStatement(query);
+            System.out.print("Enter ProductID to fetch product name (e.g. 12): ");
+            int userInput = sc.nextInt();
+            preparedStatement.setInt(1, userInput);
+            ResultSet result = preparedStatement.executeQuery();
+            result.next();
+            String productName = result.getString(2); //in the parentheses I can also put the actual name of the column
+            System.out.println("\nThe product with ProductID " + userInput + " is " + "\"" + productName + "\"");
+        } catch (Exception e) {
+            System.out.println("An error has occurred!");
+            System.out.println(e);
+        }
+    }
+}
