b2f633a62f98a8c81b227abadbb3b704d719a9d7
Gaoussou Doucoure <gaousd4@gmail.com>
2025-06-15 02:12:31 -0700

    Used JDBC to fetch data

---
+++ src/main/java/Main.java
@@ -0,0 +1,38 @@
+import java.sql.*;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        try {
+            Scanner sc = new Scanner(System.in);
+            String url = "jdbc:mysql://localhost:3306/northwind"; //URL for connection
+            String username = "root";   //DATABASE username
+            System.out.println("Enter database password: ");
+            String password = sc.nextLine();  //DATABASE password will be asked to input
+
+            String query = "SELECT * FROM northwind.products;"; //QUERY to execute
+
+            Class.forName("com.mysql.cj.jdbc.Driver"); //LOADING the DRIVER
+            Connection connection = DriverManager.getConnection(url, username, password); //creating CONNECTION to the DATABASE
+            Statement statement = connection.createStatement(); //creating STATEMENT object
+            ResultSet result = statement.executeQuery(query); //executeQuery because I'm only FETCHING
+
+            //Using the FETCHED DATA
+            System.out.println("Products Names:");
+            int count = 1;
+            while (result.next()){
+                String productName = result.getString(2);
+                System.out.println(count +".) " + productName);
+                count++;
+            }
+
+            //closing the RESOURCES
+            connection.close();
+            statement.close();
+
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+
+    }
+}
