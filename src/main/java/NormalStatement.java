56f8ea1cf7ec47d794b5fb75f9097e6f981c98e2
Gaoussou Doucoure <gaousd4@gmail.com>
2025-06-18 23:30:10 -0700

    Renamed Main.java to NormalStatement.java

--- src/main/java/NormalStatement.java
+++ src/main/java/NormalStatement.java
@@ -1,7 +1,7 @@
 import java.sql.*;
 import java.util.Scanner;
 
-public class Main {
+public class NormalStatement {
     public static void main(String[] args) {
         try {
             Scanner sc = new Scanner(System.in);
