9efaaa05d0c1c21caea73ee387a5348ebf3ec004
Gaoussou Doucoure <gaousd4@gmail.com>
2025-06-19 16:30:38 -0700

    Created a home screen

---
+++ src/main/java/HomeScreen.java
@@ -0,0 +1,12 @@
+public class HomeScreen {
+    public static void homeScreen(){
+        System.out.println("""
+                
+                What do you want to do?
+                  1) Display all products?
+                  2) Display all customers?
+                  0) Exit
+                
+                """);
+    }
+}
