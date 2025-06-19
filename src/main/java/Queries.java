dc2fb75fc2bdec93dccd7e072db5888c38f049d4
Gaoussou Doucoure <gaousd4@gmail.com>
2025-06-19 16:30:05 -0700

    Added displayAllProducts and displayAllCustomers methods

--- src/main/java/Queries.java
+++ src/main/java/Queries.java
@@ -1,3 +1,11 @@
 public class Queries {
 
+    public static void displayAllProducts(){
+        String query = "SELECT * FROM northwind.products;";
+    }
+
+    public static void displayAllCustomers(){
+        String query = "SELECT ContactName, CompanyName, City, Country, Phone FROM northwind.customers;";
+    }
+
 }
