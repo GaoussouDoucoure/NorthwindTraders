5b49c2cc6df06f9f0d1808202e4887db4fb722e9
Gaoussou Doucoure <gaousd4@gmail.com>
2025-06-19 18:11:03 -0700

    Renamed HomeScreen.java to Functions to add more methods in there, on top the homeScreen method

---
+++ src/main/java/Functions.java
@@ -0,0 +1,40 @@
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class Functions {
+    //Home Screen Method
+    public static void homeScreen(){
+        System.out.println("""
+                
+                What do you want to do?
+                  1) Display all products?
+                  2) Display all customers?
+                  0) Exit
+                
+                """);
+    }
+
+    // created a method for the final block to clean up the code a bit
+    public static void closeResources(ResultSet resultSet, PreparedStatement preparedStatement, Connection connection) {
+        try {
+            if (resultSet != null) resultSet.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            if (preparedStatement != null) preparedStatement.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            if (connection != null) connection.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
