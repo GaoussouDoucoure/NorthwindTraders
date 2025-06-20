5f42c22abe31c10d36dd64e6ec1261691d8e3392
Gaoussou Doucoure <gaousd4@gmail.com>
2025-06-19 17:34:33 -0700

    Implemented the Main to give the user options of what to do like displaying all products or customers

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,69 +3,125 @@ import java.sql.PreparedStatement;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ClassNotFoundException {
         Scanner sc = new Scanner(System.in);
-        String url = "jdbc:mysql://localhost:3306/northwind";
-        System.out.print("Enter database username: ");
-        String username = sc.nextLine();
-        System.out.print("Enter database password: ");
-        String password = sc.nextLine();
-        String query = """
-                SELECT ProductID, ProductName, UnitPrice, UnitsInStock
-                FROM northwind.products;
-                """;
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+        ResultSet resultSet = null;
+        String query = "";
 
-        try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
-            Connection con = DriverManager.getConnection(url, username, password);
-            PreparedStatement prepStatement = con.prepareStatement(query);
-            ResultSet rs = prepStatement.executeQuery();
+        HomeScreen.homeScreen();
+        System.out.print("Select an option: ");
+        int choice = sc.nextInt();
+        switch (choice){
+            case 0:
+                System.out.println("\nYou chose to exit the application. Application terminated..");
+                break;
 
-            /*
-            FIRST OUTPUT OPTION in the book EXERCISE 2 Page-46 JDBC. 2ND OUTPUT OPTION after this COMMENTED SECTION
+            case 1:
+                query = Queries.displayAllProducts();
 
-            while (rs.next()){
-                int productId = rs.getInt("ProductID");
-                String productName = rs.getString("ProductName");
-                double unitPrice = rs.getDouble("UnitPrice");
-                int unitsInStock = rs.getInt("UnitsInStock");
+                String url = "jdbc:mysql://localhost:3306/northwind";
+                sc.nextLine();
+                System.out.print("Enter database username: ");
+                String username = sc.nextLine();
+                System.out.print("Enter database password: ");
+                String password = sc.nextLine();
 
-                System.out.printf("""
-                    
-                    Product Id:      %d
-                    Name:            %s
-                    Price:           $%.2f
-                    Stock:           %d
-                    ------------------------""",
-                    productId, productName, unitPrice, unitsInStock);
-            }
+                try {
+                    connection = DriverManager.getConnection(url, username, password);
+                    preparedStatement = connection.prepareStatement(query);
+                    resultSet = preparedStatement.executeQuery();
 
-             */
+                    System.out.println("\nProducts Names:");
+                    int count = 1;
+                    while (resultSet.next()){
+                        String productName = resultSet.getString(2);
+                        System.out.println(count +".) " + productName);
+                        count++;
+                    }
 
-            //SECOND OUTPUT OPTION
-            // Print header with pipes
-            System.out.printf("| %-8s | %-35s | %-8s | %-5s |%n", "Id", "Name", "Price", "Stock");
-            System.out.println("|----------|-------------------------------------|----------|-------|");
+                }
+                catch (SQLException e) {
+                    e.printStackTrace();
+                }
+                finally {
+                    // close the resources
+                    closeResources(resultSet, preparedStatement, connection);
+                }
 
-            // Print each product
-            while (rs.next()) {
-                int id = rs.getInt("ProductID");
-                String name = rs.getString("ProductName");
-                double price = rs.getDouble("UnitPrice");
-                int stock = rs.getInt("UnitsInStock");
+                break;
 
-                System.out.printf("| %-8d | %-35s | $%-7.2f | %5d |%n", id, name, price, stock);
-            }
+            case 2:
+                query= Queries.displayAllCustomers();
 
-            rs.close();
-            prepStatement.close();
-            con.close();
+                url = "jdbc:mysql://localhost:3306/northwind";
+                sc.nextLine();
+                System.out.print("Enter database username: ");
+                username = sc.nextLine();
+                System.out.print("Enter database password: ");
+                password = sc.nextLine();
 
-        } catch (Exception e) {
-            System.out.println(e);
-            throw new RuntimeException(e);
+                try {
+                    connection = DriverManager.getConnection(url, username, password);
+                    preparedStatement = connection.prepareStatement(query);
+                    resultSet = preparedStatement.executeQuery();
+
+                    while (resultSet.next()){
+                        String contactName = resultSet.getString("ContactName");
+                        String companyName = resultSet.getString("CompanyName");
+                        String city = resultSet.getString("City");
+                        String country = resultSet.getString("Country");
+                        String phone = resultSet.getString("Phone");
+                        System.out.printf("""
+                                
+                                Contact Name: %s
+                                Company Name: %s
+                                City: %s
+                                Country: %s
+                                Phone: %s
+                                ----------------------------------------
+                                """, contactName, companyName, city, country, phone);
+                    }
+
+                }
+                catch (SQLException e) {
+                    e.printStackTrace();
+                }
+                finally {
+                    // close the resources
+                    closeResources(resultSet, preparedStatement, connection);
+                }
+
+                break;
+            default:
+                System.out.println("You have entered the wrong choice: " + choice);
+                HomeScreen.homeScreen();
         }
 
     }
+
+    // created a method for the final block to clean up the code a bit
+    public static void closeResources(ResultSet resultSet, PreparedStatement preparedStatement, Connection connection) {
+        try {
+            if (resultSet != null) resultSet.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            if (preparedStatement != null) preparedStatement.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            if (connection != null) connection.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
 
 }
